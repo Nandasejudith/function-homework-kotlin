c20c9f460b4e25101b8751e58b191d59c378e521
Nandasejudith <judithnandase@gmail.com>
2021-02-28 15:13:59 +0300

    functions homework

---
+++ src/functions  homework.kt
@@ -0,0 +1,19 @@
+fun main() {
+    name()
+    println(25%4)
+    println(2+4+6+8)
+    fact()
+}
+fun name(){
+    var name ="Nandase Judith"
+    println("Hello $name")
+}
+fun remaider(a:Int,b:Int):Int{
+    return(a%b)
+}
+fun add(num1:Int, num2:Int, num3:Int, num4:Int):Int{
+    return(num1+num2+num3+num4)
+}
+fun fact(){
+    println("I love peace.")
+}
